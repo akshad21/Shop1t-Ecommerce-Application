8bc4647925abefc8346a8f0dfca8ef5f7a9d3936
Akshad <akshad.satkar@gmail.com>
2025-01-21 15:42:58 +0530

    test commit

--- src/components/category_card.jsx
+++ src/components/category_card.jsx
@@ -10,7 +10,7 @@ const Card = ({ title, imageSrc }) => {
           src={imageSrc}
           alt={title}
           layout="fill"
-          objectFit="cover"
+          objectFit="contain"
           className="group-hover:scale-105 transition-transform duration-300"
         />
       </div>
